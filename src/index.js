43d80320e0285f2a953258bbdd879f79aeb675d3
argon1025 <argon1025@gmail.com>
2021-02-19 15:11:53 +0900

    ADD - Docker 테스트파일을 업로드 했습니다.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const PORT = 8001;
+const HOST = "localhost";
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+/*
+if(process.env.NODE_PORT&&process.env.NODE_HOST){
+    const PORT = process.env.NODE_PORT;
+    const HOST = process.env.NODE_HOST;
+    app.listen(PORT, HOST);
+    console.log(`Running on http://${HOST}:${PORT}`);
+}else{
+    const PORT = 8080;
+    const HOST = "localhost";
+    app.listen(PORT, HOST);
+    console.log(`Running on dev http://${HOST}:${PORT}`);
+}
+*/
