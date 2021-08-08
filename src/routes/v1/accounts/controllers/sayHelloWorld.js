1f566d41365709308942fa37f6bde3ad276f73fa
argon1025 <argon1025@gmail.com>
2021-08-08 19:00:13 +0900

    Init Project

--- src/routes/v1/accounts/controllers/sayHelloWorld.js
+++
@@ -1,3 +0,0 @@
-module.exports = (req, res, next) => {
-    res.status(200).json("accounts controller test!");
-};
--- src/routes/v1/posts/controllers/sayHelloWorld.js
+++
@@ -1,3 +0,0 @@
-module.exports = (req, res, next) => {
-    res.status(200).json("posts controller test!");
-};
