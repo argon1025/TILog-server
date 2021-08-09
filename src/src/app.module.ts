ce6874e59182327a7cf91fafad5fcdd70c988c58
argon1025 <argon1025@gmail.com>
2021-08-09 20:27:15 +0900

    Feature TIL-25 Logging middleware

--- src/src/app.module.ts
+++ src/src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/Logger.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+// Add Middleware
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
