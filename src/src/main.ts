1eebb10b7a456b29067aa7f39d90b4e51ca3fe77
argon1025 <argon1025@gmail.com>
2021-08-09 21:02:46 +0900

    TIL-26 Add .env Config load

--- src/src/main.ts
+++ src/src/main.ts
@@ -1,8 +1,23 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Configuration Load
+  const configService = app.get<ConfigService>(ConfigService);
+  const SERVER_PORT = configService.get<number>('SERVER_PORT', null);
+  const SERVER_ENV = configService.get<string>('NODE_ENV', null);
+  const SERVER_HOST = configService.get<string>('SERVER_HOST', null);
+
+  if (!SERVER_ENV || !SERVER_PORT || !SERVER_HOST) {
+    Logger.error('Unable to load environment variables!');
+    throw new Error('Unable to load environment variables!');
+  } else {
+    await app.listen(SERVER_PORT);
+    Logger.log(`--------->>> ${SERVER_ENV}://${SERVER_HOST}:${SERVER_PORT} <<<---------`, 'SERVER_INFO');
+  }
 }
 bootstrap();
