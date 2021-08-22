6788877bdc6aadc8652df77bc3d372199a454951
argon1025 <argon1025@gmail.com>
2021-08-22 19:20:35 +0900

    TIL-43 Posts 리소스를 생성하라

---
+++ src/src/posts/posts.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postsService.create(createPostDto);
+  }
+}
