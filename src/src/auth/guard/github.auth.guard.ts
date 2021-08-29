3b29ed0d58d7d7ee5cd3012e45327be65f088b88
MINJE-98 <jmj012100@gmail.com>
2021-08-29 11:42:12 +0900

    TIL-40 GithubGuard, AuthenticatedGuard 작성하라

---
+++ src/src/auth/guard/github.auth.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+
+//  Github Strategy에서 데이터 검증이 완료 된 후  super.login()메소드가 serializeUser를 호출하여 세션에 유저 정보를 저장 후 반환됩니다.
+export class GithubGuard extends AuthGuard('github') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const activate = (await super.canActivate(context)) as boolean;
+    const request = context.switchToHttp().getRequest();
+    await super.logIn(request);
+    console.log('GithubGuard');
+    return activate;
+  }
+}
+// 인증 유무를 반환합니다
+@Injectable()
+export class AuthenticatedGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    console.log('AuthenticatedGuard');
+    const req = context.switchToHttp().getRequest();
+    return req.isAuthenticated();
+  }
+}
