bad7d1f8c669f56d4352fe709ddfa9dec5e9041e
argon1025 <argon1025@gmail.com>
2021-08-29 22:38:28 +0900

    TIL-46 modifyPostByPostID 로직을 작성하라

--- src/src/posts/posts.service.ts
+++ src/src/posts/posts.service.ts
@@ -49,8 +49,65 @@ export class PostsService {
       await queryRunner.release();
     }
   }
-  // 게시글 수정
-  public modifyPostByPostID(personalRequest: boolean, postID: number, categoryID: number, title: string, thumbnailURL: string, markDownContent: string, isPrivate: boolean) {}
+
+  /**
+   * 게시글 수정
+   * @param personalRequest
+   * @param postID
+   * @param categoryID
+   * @param title
+   * @param thumbnailURL
+   * @param markDownContent
+   * @param isPrivate
+   * @returns Promise<boolean>
+   *
+   * 1. 사용자 본인의 요청인지 확인합니다.-> 향후 해당 로직을 컨트롤러로 분리할 수 있습니다.
+   * 2. 게시글을 수정합니다.
+   *
+   */
+  public async modifyPostByPostID(personalRequest: boolean, postID: number, categoryID: number, title: string, thumbnailURL: string, markDownContent: string, isPrivate: boolean | number) {
+    // 사용자 본인의 요청인지 확인합니다.
+    if (!personalRequest) {
+      return false;
+    }
+    // 생성일자 반환
+    const NOW_DATE = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    // Mysql TinyInt로 Boolean타입 변환
+    isPrivate = isPrivate ? 1 : 0;
+
+    // 새 커넥션과 쿼리 러너객체를 생성합니다
+    const connection = getConnection();
+    const queryRunner = connection.createQueryRunner();
+
+    // 데이터 베이스에 연결합니다
+    await queryRunner.connect();
+
+    // 트랜잭션을 시작합니다.
+    await queryRunner.startTransaction();
+
+    try {
+      // 쿼리빌더를 통해 UPDATE 구문을 실행합니다.
+      await queryRunner.manager
+        .createQueryBuilder()
+        .useTransaction(true)
+        .update(Posts)
+        .set({ categoryId: categoryID, title: title, thumbNaillUrl: thumbnailURL, markDownContent: markDownContent, private: isPrivate, updatedAt: NOW_DATE })
+        .where('id = :postID', { postID: postID })
+        .updateEntity(false)
+        .execute();
+
+      // 변경 사항을 커밋합니다.
+      await queryRunner.commitTransaction();
+      return true;
+    } catch (error) {
+      // 롤백을 실행합니다.
+      await queryRunner.rollbackTransaction();
+      return false;
+    } finally {
+      // 생성된 커넥션을 해제합니다.
+      await queryRunner.release();
+    }
+  }
   // 게시글 리스트 리턴
   public getPostsByUserID(personalRequest: boolean, userID: number, cursorNumber: number, contentLimit: number) {}
   // 게시글 상세보기
