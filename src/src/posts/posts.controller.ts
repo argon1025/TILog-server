5148a972c511a0fbaebf25fb21f80a391628409c
argon1025 <argon1025@gmail.com>
2021-08-30 02:13:51 +0900

    TIL-47 TIL-52 필요없는 DTO를 삭제하라

--- src/src/posts/posts.controller.ts
+++ src/src/posts/posts.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postsService.create(createPostDto);
+  async create(@Body() createPostDto: any) {
+    await this.postsService.getPostsByUserID(true, 1, 0, 10);
+    return 'this.postsService.create(createPostDto)';
   }
 }
