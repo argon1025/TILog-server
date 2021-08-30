484e81be49a9fda5d337fb432f31464fb83e40a2
MINJE-98 <jmj012100@gmail.com>
2021-08-30 15:59:53 +0900

    TIL-40 authController에 주 석을 추가고, CustomDecorator을 작성하여 수정하라

--- src/src/auth/auth.controller.ts
+++ src/src/auth/auth.controller.ts
@@ -1,35 +1,44 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Redirect, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { GithubGuard } from './guard/github.auth.guard';
+import { RedirectClient } from './decorators/redirect.decorator';
+import { Session } from './decorators/session.decorator';
+import { UserStats } from './decorators/userStats.decorator';
+import { AuthenticatedGuard, GithubGuard } from './guard/github.auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  // github 로그인을 요청합니다.
   @Get('github')
   @UseGuards(GithubGuard)
   login() {
     return;
   }
 
+  // github 로그인이 성공적으로 완료되면 callback주소로 리다이렉트 됩니다.
   @Get('github/callback')
   @UseGuards(GithubGuard)
-  callback(@Res() res) {
-    res.redirect('http://localhost:8080');
+  callback(@RedirectClient() redirect) {
+    return redirect;
   }
 
+  // 로그인한 유저 정보를 반환합니다.
   @Get('status')
-  status(@Req() req) {
-    console.log('STATUS');
-
-    return req.user;
+  @UseGuards(AuthenticatedGuard)
+  status(@UserStats() userStats) {
+    return userStats;
   }
+
+  // 로그인한 유저의 세션을 파기시킵니다.
   @Get('logout')
-  logout(@Req() req, @Res() res) {
-    req.session.destroy((err) => {
-      if (err) {
-        return console.log(err);
+  @UseGuards(AuthenticatedGuard)
+  logout(@Session() session, @RedirectClient() redirect) {
+    // 세션을 제거합니다.
+    session.destroy((error) => {
+      if (error) {
+        return console.log(error);
       }
-      res.redirect('http://localhost:8080');
+      return redirect;
     });
   }
 }
