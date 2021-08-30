5248630631296b1a85f8877888c8afe33fedc94c
MINJE-98 <jmj012100@gmail.com>
2021-08-30 15:56:21 +0900

    TIL-40 init.sal에 nullable옵션을 수정하라

--- docker/mysql/mysql-init-files/init.sql
+++ docker/mysql/mysql-init-files/init.sql
@@ -24,9 +24,9 @@ CREATE TABLE users
     `mailAddress`      VARCHAR(50)     NULL        COMMENT '서비스 메일 주소', 
     `password`         VARCHAR(50)     NULL        COMMENT '사용자 암호, Oauth 사용자는 NULL', 
     `accessToken`         VARCHAR(255)   NOT  NULL        COMMENT '서비스 엑세스 토큰', 
-    `createdAt`        DATETIME        NOT NULL    COMMENT '유저 가입일', 
-    `updatedAt`        DATETIME        NOT NULL    COMMENT '유저 갱신일', 
-    `deletedAt`        DATETIME        NOT NULL    COMMENT '유저 삭제일', 
+    `createdAt`        DATETIME    NOT  NULL    COMMENT '유저 가입일', 
+    `updatedAt`        DATETIME        NULL    COMMENT '유저 갱신일', 
+    `deletedAt`        DATETIME        NULL    COMMENT '유저 삭제일', 
     `admin`            TINYINT         NOT NULL    DEFAULT 0 COMMENT '관리자 유무', 
     CONSTRAINT PK_users PRIMARY KEY (id)
 );
