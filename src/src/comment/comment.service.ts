926dc958a45eb43eea6a1ca6496bf1c09a5bcaec
MINJE-98 <jmj012100@gmail.com>
2021-09-10 13:44:27 +0900

    TIL-55 service에 TypeORM을연결하라.

--- src/src/comment/comment.service.ts
+++ src/src/comment/comment.service.ts
@@ -1,26 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comments } from 'src/entities/Comments';
+import { Users } from 'src/entities/Users';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CommentService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
-  }
-
-  findAll() {
-    return `This action returns all comment`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
-  }
-
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
-  }
+  constructor(@InjectRepository(Comments) private commentsRepo: Repository<Comments>) {}
 }
