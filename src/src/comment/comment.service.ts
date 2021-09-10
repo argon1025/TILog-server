ba53a4c4e157d9b6063453670dad9fe85d50649b
MINJE-98 <jmj012100@gmail.com>
2021-09-10 15:17:03 +0900

    TIL-56 createReplyComment의 로직을 작성하라.

--- src/src/comment/comment.service.ts
+++ src/src/comment/comment.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Comments } from 'src/entities/Comments';
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
+import { CreateReplyComment } from './dto/createReplyComment.dto';
 
 @Injectable()
 export class CommentService {
@@ -20,6 +21,28 @@ export class CommentService {
       postsId: postID,
       htmlContent: contents,
       replyLevel: 0,
+      createdAt: now,
+    });
+  }
+  /**
+   *
+   * @param userID 유저 아이디
+   * @param postID 포스트 아이디
+   * @param replyTo 상위 코멘트 아이디
+   * @param commentInfo 코멘트 내용 및 상위 댓글 여부
+   * @returns
+   * @TODO error handling
+   */
+  async createReplyComment(userID: number, postID: string, replyTo: string, commentInfo: CreateReplyComment) {
+    const now = new Date();
+    const { contents, replyLevel } = commentInfo;
+    if (replyLevel == 1) return 'error replyLevel is 1';
+    return await this.commentsRepo.save({
+      usersId: userID,
+      postsId: postID,
+      htmlContent: contents,
+      replyTo: replyTo,
+      replyLevel: 1,
       createdAt: now,
     });
   }
