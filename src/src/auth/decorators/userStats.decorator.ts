126f9c807d018e7db6417ace4de2649b008e7ed1
MINJE-98 <jmj012100@gmail.com>
2021-09-10 13:34:51 +0900

    TIL-55 userstats 데코레이터를 필요한 데이터만 반환하도록 수정하라.

--- src/src/auth/decorators/userStats.decorator.ts
+++ src/src/auth/decorators/userStats.decorator.ts
@@ -1,7 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 // 유저 정보를 반환합니다.
-export const UserStats = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+export const UserStats = createParamDecorator((data: string, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
-  return request.user;
+  const user = request.user;
+  return data ? user?.[data] : user;
 });
