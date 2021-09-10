e36d086351d11db3476da3534045895ed8e0db58
MINJE-98 <jmj012100@gmail.com>
2021-09-10 13:45:02 +0900

    TIL-55 createNewComment 로직을 작성하라.

--- src/src/comment/comment.service.ts
+++ src/src/comment/comment.service.ts
@@ -7,4 +7,20 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class CommentService {
   constructor(@InjectRepository(Comments) private commentsRepo: Repository<Comments>) {}
+  /**
+   * @param userID 유저 아이디
+   * @param postID 포스트 아이디
+   * @param contents 코멘트 내용
+   * @returns
+   */
+  async createComment(userID: number, postID: string, contents: string) {
+    const now = new Date();
+    return await this.commentsRepo.save({
+      usersId: userID,
+      postsId: postID,
+      htmlContent: contents,
+      replyLevel: 0,
+      createdAt: now,
+    });
+  }
 }
