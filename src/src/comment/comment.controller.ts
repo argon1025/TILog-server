1757dde2a49686699997b385542e176da69fb6c2
MINJE-98 <jmj012100@gmail.com>
2021-09-10 15:16:47 +0900

    TIL-56 createReplyComment함수를 작성하라.

--- src/src/comment/comment.controller.ts
+++ src/src/comment/comment.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Res, Req 
 import { UserStats } from 'src/auth/decorators/userStats.decorator';
 import { AuthenticatedGuard } from 'src/auth/guard/github.auth.guard';
 import { CommentService } from './comment.service';
+import { CreateReplyComment } from './dto/createReplyComment.dto';
 
 @Controller('comment')
 export class CommentController {
@@ -14,5 +15,14 @@ export class CommentController {
   @UseGuards(AuthenticatedGuard)
   createNewComment(@UserStats('id') userID: number, @Param('postid') postID: string, @Body('contents') contents: string) {
     return this.commentService.createNewComment(userID, postID, contents);
+  }
+  /**
+   * 대댓글을 생성합니다.
+   * *인증된 유저만 코멘트를 작성할 수 있습니다.
+   */
+  @Post('post/:postid/comment/:replyto')
+  @UseGuards(AuthenticatedGuard)
+  createReplyComment(@UserStats('id') userID: number, @Param('postid') postID: string, @Param('replyto') replyTo: string, @Body('commentinfo') commentInfo: CreateReplyComment) {
+    return this.commentService.createReplyComment(userID, postID, replyTo, commentInfo);
   }
 }
