67aa6c703d31b276eac537ecfc79ea7a51bfb2ea
MINJE-98 <jmj012100@gmail.com>
2021-10-25 19:58:04 +0900

    TIL-71 controller 사용하지않는 메서드 삭제하라

--- src/src/users/users.controller.ts
+++ src/src/users/users.controller.ts
@@ -4,29 +4,4 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create() {
-    return this.usersService.create();
-  }
-
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string) {
-    return this.usersService.update(+id);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
 }
