443bd53e1d89fa4d4bb14038e252fd083baa6852
MINJE-98 <jmj012100@gmail.com>
2021-10-25 17:29:11 +0900

    TIL-69 githubStragy 작성하라

---
+++ src/src/auth/strategies/github.strategy.ts
@@ -0,0 +1,31 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-github2';
+import { Injectable } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+import { userinfo } from '../dto/userinfo.dto';
+
+@Injectable()
+export class GithubStrategy extends PassportStrategy(Strategy, 'github') {
+  constructor(private readonly authService: AuthService) {
+    super({
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL,
+      scope: ['profile'],
+    });
+  }
+  // 유저의 자격증명이 완료되면 유저의 정보를 받고, 유저 데이터를 검증 후 반환합니다.
+  async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+    const { nodeId, username, profileUrl, provider } = profile;
+    const now = new Date();
+    const userinfo: userinfo = {
+      oAuthServiceId: nodeId,
+      userName: username,
+      proFileImageURL: profileUrl,
+      oAuthType: provider,
+      accessToken: accessToken,
+      createdAt: now,
+    };
+    return await this.authService.validateUser(userinfo);
+  }
+}
