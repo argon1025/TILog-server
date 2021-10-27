2977b604acb1d625541c9bab87f1983b40827996
MINJE-98 <jmj012100@gmail.com>
2021-10-27 19:54:15 +0900

    TIL-79 기존 session-info DTO를 생성하라

---
+++ src/src/auth/dto/session-info.dto.ts
@@ -0,0 +1,10 @@
+export class SessionInfo {
+  id: number;
+  oAuthType: string;
+  oAuthServiceId: string;
+  userName: string;
+  mailAddress: string;
+  createdAt: Date;
+  updatedAt: Date;
+  admin: number;
+}
