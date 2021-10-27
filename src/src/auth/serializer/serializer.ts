5b6fddbf2cd88375e8e33b44f085ba5b0a4e352a
MINJE-98 <jmj012100@gmail.com>
2021-10-27 19:55:08 +0900

    TIL-79 기존 변경된 DTO에 맞게 로직을 수정하라

--- src/src/auth/serializer/serializer.ts
+++ src/src/auth/serializer/serializer.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { Users } from 'src/entities/Users';
 import { UsersService } from 'src/users/users.service';
 import { Done } from '../dto/done.dto';
+import { SessionInfo } from '../dto/session-info.dto';
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
@@ -11,13 +12,15 @@ export class SessionSerializer extends PassportSerializer {
   }
 
   // Stretegy 에서 반환된 유저 정보를 세션에 저장합니다.
-  serializeUser(users: Users, done: Done) {
+  serializeUser(users: SessionInfo, done: Done) {
     console.log('serializeUser');
+    console.log(users);
+
     done(null, users);
   }
 
   // 클라이언트에서 인증이 필요한 요청을 할 때,  요청받은 세션의 유저정보가 DB에 유저 정보를 확인 후 done으로 유저 정보를 반환해줍니다.
-  async deserializeUser(users: Users, done: Done) {
+  async deserializeUser(users: SessionInfo, done: Done) {
     console.log('deserializeUser');
     const userDB = await this.usersService.findUser(users.oAuthServiceId);
     return userDB ? done(null, userDB) : done(null, null);
--- src/src/users/users.service.ts
+++ src/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { userinfo } from 'src/auth/dto/userinfo.dto';
+import { SessionInfo } from 'src/auth/dto/session-info.dto';
+import { UserInfo } from 'src/auth/dto/userinfo.dto';
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
 
@@ -8,12 +9,15 @@ import { Repository } from 'typeorm';
 export class UsersService {
   constructor(@InjectRepository(Users) private userRepo: Repository<Users>) {}
   // 유저를 생성합니다.
-  async createUser(userInfo: userinfo) {
+  async createUser(userInfo: UserInfo) {
     const user = await this.userRepo.create(userInfo);
     return await this.userRepo.save(user);
   }
   // 특정 유저를 검색합니다.
   async findUser(oAuthServiceId: string): Promise<Users | undefined> {
-    return await this.userRepo.findOne({ oAuthServiceId });
+    return await this.userRepo.findOne({
+      select: ['id', 'oAuthType', 'oAuthServiceId', 'userName', 'mailAddress', 'createdAt', 'updatedAt', 'admin'],
+      where: { oAuthServiceId },
+    });
   }
 }
