dbeed961dd960a538699d4da7a24059aa852f8ae
MINJE-98 <jmj012100@gmail.com>
2021-10-27 19:55:41 +0900

    TIL-79 Module에 Serializer를 추가하라

--- src/src/auth/auth.module.ts
+++ src/src/auth/auth.module.ts
@@ -5,11 +5,12 @@ import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
 import { UsersService } from 'src/users/users.service';
+import { SessionSerializer } from './serializer/serializer';
 import { GithubStrategy } from './strategies/github.strategy';
 
 @Module({
   imports: [PassportModule, TypeOrmModule.forFeature([Users])],
   controllers: [AuthController],
-  providers: [AuthService, UsersService, GithubStrategy],
+  providers: [AuthService, UsersService, SessionSerializer, GithubStrategy],
 })
 export class AuthModule {}
