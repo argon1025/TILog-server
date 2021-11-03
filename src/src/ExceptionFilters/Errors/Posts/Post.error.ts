43ec2f9a472e32378c5daab98caf6a6f6d22cfd2
argon1025 <argon1025@gmail.com>
2021-11-03 20:08:32 +0900

    Add createPost service

--- src/src/ExceptionFilters/Errors/Posts/Post.error.ts
+++ src/src/ExceptionFilters/Errors/Posts/Post.error.ts
@@ -33,3 +33,20 @@ export class PostWriterNotFound implements Error {
     kr: '포스트 작성자를 찾을 수 없습니다.',
   };
 }
+
+// 포스트 생성에 실패
+export class PostCreateFail implements Error {
+  // 개발자 코멘트를 생성자 매개변수로 할당할 수 있다.
+  constructor(public readonly description?) {}
+
+  // 미리 정의된 에러코드
+  public readonly codeNumber = 400;
+
+  public readonly codeText = 'PostCreateFail';
+
+  // 미리 정의된 메시지 객체
+  public readonly message = {
+    en: 'fail to create post',
+    kr: '포스트를 생성하는데 실패했습니다.',
+  };
+}
--- src/src/posts/posts.service.ts
+++ src/src/posts/posts.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { Posts } from '../entities/Posts';
-import { PostWriterNotFound } from '../ExceptionFilters/Errors/Posts/Post.error';
+import { PostCreateFail, PostWriterNotFound } from '../ExceptionFilters/Errors/Posts/Post.error';
 import { Connection } from 'typeorm';
 import { GetPostWriterDto, GetPostWriterResponseDto } from './dto/Services/GetPostWriter.DTO';
+import { CreatePostDto } from './dto/Services/CreatePost.DTO';
+import Time from '../utilities/Time.utility';
 
 @Injectable()
 export class PostsService {
@@ -13,7 +15,7 @@ export class PostsService {
    * @author seongrokLee <argon1025@gmail.com>
    * @version 1.0.0
    */
-  public async getPostWriterId(getPostWriterData: GetPostWriterDto): Promise<GetPostWriterResponseDto | PostWriterNotFound> {
+  public async getPostWriterId(postWriterData: GetPostWriterDto): Promise<GetPostWriterResponseDto | PostWriterNotFound> {
     // 쿼리러너 객체 생성
     const queryRunner = this.connection.createQueryRunner();
 
@@ -29,7 +31,7 @@ export class PostsService {
         .createQueryBuilder()
         .select('postTable.usersId')
         .from(Posts, 'postTable')
-        .where('postTable.id = :postId', { postId: getPostWriterData.id })
+        .where('postTable.id = :postId', { postId: postWriterData.id })
         .maxExecutionTime(1000);
 
       /**
@@ -44,6 +46,7 @@ export class PostsService {
       // 트랜잭션 커밋
       await queryRunner.commitTransaction();
 
+      // return
       return resultData;
     } catch (error) {
       // 트랜잭션 롤백
@@ -61,7 +64,65 @@ export class PostsService {
    * @author seongrokLee <argon1025@gmail.com>
    * @version 1.0.0
    */
-  public async createPost() {}
+  public async createPost(postData: CreatePostDto) {
+    // 쿼리러너 객체 생성
+    const queryRunner = this.connection.createQueryRunner();
+
+    // 데이터 베이스 연결
+    await queryRunner.connect();
+
+    // 트랜잭션 시작
+    await queryRunner.startTransaction();
+
+    try {
+      // 쿼리 작성
+      const query = queryRunner.manager
+        .createQueryBuilder()
+        .insert()
+        .into(Posts)
+        .values([
+          {
+            usersId: postData.usersId,
+            categoryId: postData.categoryId,
+            title: postData.title,
+            thumbNailUrl: postData.thumbNailUrl,
+            markDownContent: postData.markDownContent,
+            private: postData.private,
+            createdAt: Time.nowDate(),
+          },
+        ])
+        .updateEntity(false);
+
+      /**
+       * @Returns Posts InsertResult {
+       * identifiers: [],
+       * generatedMaps: [],
+       * raw: ResultSetHeader {
+       *   fieldCount: 0,
+       *   affectedRows: 1,
+       *   insertId: 5,
+       *   info: '',
+       *   serverStatus: 3,
+       *   warningStatus: 0
+       *  }
+       * }
+       */
+      const queryResult = await query.execute();
+
+      // 트랜잭션 커밋
+      await queryRunner.commitTransaction();
+      // return
+      return true;
+    } catch (error) {
+      // 트랜잭션 롤백
+      await queryRunner.rollbackTransaction();
+      // 에러
+      throw new PostCreateFail('service.post.createPost 에러입니다.');
+    } finally {
+      // 데이터베이스 커넥션 해제
+      await queryRunner.release();
+    }
+  }
 
   /**
    * 포스트를 수정 합니다.
