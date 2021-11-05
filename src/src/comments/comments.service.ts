def1fb3af9add976814883a9f0f4d78f97da5ad9
MINJE-98 <jmj012100@gmail.com>
2021-11-05 20:39:07 +0900

    TIL-73 writePostComments.dto에 맞게 로직 변경

--- src/src/comments/comments.service.ts
+++ src/src/comments/comments.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Comments } from 'src/entities/Comments';
 import { Repository } from 'typeorm/repository/Repository';
+import { writePostCommentDTO } from './dto/service/writePostComments.dto';
 @Injectable()
 export class CommentsService {
   constructor(@InjectRepository(Comments) private commentsRepo: Repository<Comments>) {}
@@ -9,14 +10,33 @@ export class CommentsService {
   //////////////////// WriteComments
 
   // 코멘트 작성
-  async writePostComment(userID: number, postID: string, contents: string): Promise<Comments> {
+  async writePostComment(writePostCommentDto: writePostCommentDTO): Promise<Comments> {
     try {
+      const { userID, postID, contents } = writePostCommentDto;
       return await this.commentsRepo.save({
         usersId: userID,
         postsId: postID,
         htmlContent: contents,
         replyLevel: 0,
         createdAt: 0,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+  //  리플 작성
+  async writeReplyComment(userID: number, postID: string, replyTo: string, commentInfo: any) {
+    try {
+      const now = new Date();
+      const { contents, replyLevel } = commentInfo;
+      if (replyLevel != 0) throw new Error('대댓을 작성할 수 없습니다.');
+      return await this.commentsRepo.save({
+        usersId: userID,
+        postsId: postID,
+        htmlContent: contents,
+        replyTo: replyTo,
+        replyLevel: 1,
+        createdAt: now,
       });
     } catch (error) {
       throw new Error(error);
