691b7747d0d3f42c4beaa04c26fe44ebb49c9822
MINJE-98 <jmj012100@gmail.com>
2021-11-05 19:41:44 +0900

    TIL-73 writePostComment 로직 작성

--- src/src/comments/comments.service.ts
+++ src/src/comments/comments.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comments } from 'src/entities/Comments';
+import { Repository } from 'typeorm/repository/Repository';
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(@InjectRepository(Comments) private commentsRepo: Repository<Comments>) {}
+
+  //////////////////// WriteComments
+
+  // 코멘트 작성
+  async writePostComment(userID: number, postID: string, contents: string): Promise<Comments> {
+    try {
+      return await this.commentsRepo.save({
+        usersId: userID,
+        postsId: postID,
+        htmlContent: contents,
+        replyLevel: 0,
+        createdAt: 0,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
