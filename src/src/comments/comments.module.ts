189bae1ce4bc797363dec2734ec5553223ebb963
MINJE-98 <jmj012100@gmail.com>
2021-11-05 19:32:33 +0900

    TIL-73 Module에 ORM 추가

--- src/src/comments/comments.module.ts
+++ src/src/comments/comments.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comments } from 'src/entities/Comments';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Comments])],
   controllers: [CommentsController],
-  providers: [CommentsService]
+  providers: [CommentsService],
 })
 export class CommentsModule {}
