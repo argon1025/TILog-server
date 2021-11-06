a5e714887606a14d8f55e80ce1d88fc5eeba6ab8
argon1025 <argon1025@gmail.com>
2021-11-06 22:11:05 +0900

    Add FileInterceptor 테스트코드 추가

--- src/src/file-uploads/file-uploads.controller.ts
+++ src/src/file-uploads/file-uploads.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { FileUploadsService } from './file-uploads.service';
 
 @Controller('file-uploads')
 export class FileUploadsController {
   constructor(private readonly fileUploadsService: FileUploadsService) {}
 
-  @Get()
-  findAll() {
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  findAll(@UploadedFile() file: Express.Multer.File) {
+    try {
+      console.log(file);
+    } catch (error) {
+      console.log(error);
+    }
     return this.fileUploadsService.findAll();
   }
 }
