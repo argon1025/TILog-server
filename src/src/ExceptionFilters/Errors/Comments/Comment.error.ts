0e447daa0b94a62e1401e7a53735ef2b002abb69
MINJE-98 <jmj012100@gmail.com>
2021-11-06 14:26:04 +0900

    reply write error 작성

--- src/src/ExceptionFilters/Errors/Comments/Comment.error.ts
+++ src/src/ExceptionFilters/Errors/Comments/Comment.error.ts
@@ -16,3 +16,20 @@ export class CommentWriteFailed implements Error {
     kr: '코멘트 작성에 실패하였습니다.',
   };
 }
+
+// 리플 작성 실패
+export class ReplyWriteFailed implements Error {
+  // 개발자 코멘트를 생성자 매개변수로 할당할 수 있다.
+  constructor(public readonly description?) {}
+
+  // 미리 정의된 에러코드
+  public readonly codeNumber = 404;
+
+  public readonly codeText = 'ReplyWriteFailed';
+
+  // 미리 정의된 메시지 객체
+  public readonly message = {
+    en: 'reply write failed',
+    kr: '코멘트 작성에 실패하였습니다.',
+  };
+}
