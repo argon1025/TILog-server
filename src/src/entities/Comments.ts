82b05794aff6b32e2ce016f83abbd81061118321
MINJE-98 <jmj012100@gmail.com>
2021-11-09 00:18:56 +0900

    Edit comment entity self join을 위해 관계 생성

--- src/src/entities/Comments.ts
+++ src/src/entities/Comments.ts
@@ -1,76 +1,82 @@
-import {
-  Column,
-  Entity,
-  Index,
-  JoinColumn,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from "typeorm";
-import { Posts } from "./Posts";
-import { Users } from "./Users";
+import { Column, Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Posts } from './Posts';
+import { Users } from './Users';
 
-@Index("FK_comments_usersID_users_id", ["usersId"], {})
-@Index("FK_comments_postsID_posts_id", ["postsId"], {})
-@Entity("comments", { schema: "tilog" })
+@Index('FK_comments_usersID_users_id', ['usersId'], {})
+@Index('FK_comments_postsID_posts_id', ['postsId'], {})
+@Entity('comments', { schema: 'tilog' })
 export class Comments {
   @PrimaryGeneratedColumn({
-    type: "bigint",
-    name: "id",
-    comment: "코멘트 아이디",
+    type: 'bigint',
+    name: 'id',
+    comment: '코멘트 아이디',
   })
   id: string;
 
-  @Column("int", { name: "usersID", comment: "유저 아이디", unsigned: true })
+  @Column('int', { name: 'usersID', comment: '유저 아이디', unsigned: true })
   usersId: number;
 
-  @Column("bigint", { name: "postsID", comment: "포스트 아이디" })
+  @Column('bigint', { name: 'postsID', comment: '포스트 아이디' })
   postsId: string;
 
-  @Column("varchar", { name: "htmlContent", comment: "코멘트", length: 300 })
+  @Column('varchar', { name: 'htmlContent', comment: '코멘트', length: 300 })
   htmlContent: string;
 
-  @Column("bigint", {
-    name: "replyTo",
+  @Column('bigint', {
+    name: 'replyTo',
     nullable: true,
-    comment: "답글 PK, 아닐경우 NULL",
+    comment: '답글 PK, 아닐경우 NULL',
   })
   replyTo: string | null;
 
-  @Column("tinyint", {
-    name: "replyLevel",
-    comment: "루트 코멘트 판별 0,1",
+  @Column('tinyint', {
+    name: 'replyLevel',
+    comment: '루트 코멘트 판별 0,1',
     default: () => "'0'",
   })
   replyLevel: number;
 
-  @Column("datetime", { name: "createdAt", comment: "코멘트 생성일" })
+  @Column('datetime', { name: 'createdAt', comment: '코멘트 생성일' })
   createdAt: Date;
 
-  @Column("datetime", {
-    name: "updatedAt",
+  @Column('datetime', {
+    name: 'updatedAt',
     nullable: true,
-    comment: "코멘트 수정일",
+    comment: '코멘트 수정일',
   })
   updatedAt: Date | null;
 
-  @Column("datetime", {
-    name: "deletedAt",
+  @Column('datetime', {
+    name: 'deletedAt',
     nullable: true,
-    comment: "코멘트 삭제일",
+    comment: '코멘트 삭제일',
   })
   deletedAt: Date | null;
 
   @ManyToOne(() => Posts, (posts) => posts.comments, {
-    onDelete: "CASCADE",
-    onUpdate: "CASCADE",
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
   })
-  @JoinColumn([{ name: "postsID", referencedColumnName: "id" }])
+  @JoinColumn([{ name: 'postsID', referencedColumnName: 'id' }])
   posts: Posts;
 
   @ManyToOne(() => Users, (users) => users.comments, {
-    onDelete: "CASCADE",
-    onUpdate: "CASCADE",
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
   })
-  @JoinColumn([{ name: "usersID", referencedColumnName: "id" }])
+  @JoinColumn([{ name: 'usersID', referencedColumnName: 'id' }])
   users: Users;
+
+  @ManyToOne(() => Comments, (comments) => comments.parentComment, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn([{ name: 'replyTo', referencedColumnName: 'id' }])
+  parentComment: Comments;
+
+  @OneToMany(() => Comments, (comments) => comments.parentComment, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  childComment: Comments[];
 }
