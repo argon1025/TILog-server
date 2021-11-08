5497339bda738b526fcff72f325c200b5c421622
MINJE-98 <jmj012100@gmail.com>
2021-11-08 15:35:16 +0900

    Edit error 답글 작성 클래스명 수정 및 추가 답글 불가능 에러 추가

--- src/src/ExceptionFilters/Errors/Comments/Comment.error.ts
+++ src/src/ExceptionFilters/Errors/Comments/Comment.error.ts
@@ -12,28 +12,43 @@ export class CommentWriteFailed implements Error {
 
   // 미리 정의된 메시지 객체
   public readonly message = {
-    en: 'comment write failed',
+    en: 'failed to write a comment.',
     kr: '코멘트 작성에 실패하였습니다.',
   };
 }
 
-// 리플 작성 실패
-export class ReplyWriteFailed implements Error {
+// 답글 작성 실패
+export class CommentToCommentWriteFailed implements Error {
   // 개발자 코멘트를 생성자 매개변수로 할당할 수 있다.
   constructor(public readonly description?) {}
 
   // 미리 정의된 에러코드
   public readonly codeNumber = 400;
 
-  public readonly codeText = 'ReplyWriteFailed';
+  public readonly codeText = 'CommentToCommentWriteFailed';
 
   // 미리 정의된 메시지 객체
   public readonly message = {
-    en: 'reply write failed',
-    kr: '코멘트 작성에 실패하였습니다.',
+    en: 'failed to write a reply.',
+    kr: '답글 작성에 실패하였습니다.',
   };
 }
+// 추가 답글 불가능
+export class DisableLevel implements Error {
+  // 개발자 코멘트를 생성자 매개변수로 할당할 수 있다.
+  constructor(public readonly description?) {}
 
+  // 미리 정의된 에러코드
+  public readonly codeNumber = 400;
+
+  public readonly codeText = 'DisableLevel';
+
+  // 미리 정의된 메시지 객체
+  public readonly message = {
+    en: 'can not write any more comments.',
+    kr: '더이상 답글을 작성할 수 없습니다.',
+  };
+}
 // 코멘트 작성 실패
 export class ViewPostCommentsFaild implements Error {
   // 개발자 코멘트를 생성자 매개변수로 할당할 수 있다.
