0c6063629dad126a983149820b3dc5d7aa4ab109
MINJE-98 <jmj012100@gmail.com>
2021-11-09 15:53:28 +0900

    Edit 메서드 정리, viewonecomment 메서드 추가

--- src/src/comments/comments.service.ts
+++ src/src/comments/comments.service.ts
@@ -13,8 +13,11 @@ import {
   UpdateCommentFaild,
   ViewAllCommentsFaild,
   DisableLevel,
+  ViewOneCommentFaild,
+  NotCommentOwner,
 } from 'src/ExceptionFilters/Errors/Comments/Comment.error';
 import { UpdateCommentDTO } from './dto/service/updateComment.dto';
+import { DeleteCommentDTO } from './dto/service/deleteComment.dto';
 @Injectable()
 export class CommentsService {
   constructor(private connection: Connection, @InjectRepository(Comments) private commentsRepo: Repository<Comments>) {}
@@ -23,9 +26,6 @@ export class CommentsService {
    * write New comment on post
    * 포스트에 새로운 코멘트를 작성합니다.
    * @returns Comments
-   * Revert
-   * no contents
-   * no postID
    */
   async writeNewCommentOnPost(reqData: WriteNewCommentOnPostDTO): Promise<Comments> {
     try {
@@ -43,25 +43,10 @@ export class CommentsService {
   }
 
   /**
-   * vaildate comment level
-   * 코멘트의 레벨을 검증합니다.
-   * @returns boolean
-   */
-  async vaildateCommentLevel(commentID: string): Promise<void> {
-    try {
-      const { replyLevel } = await this.commentsRepo.findOne({
-        id: commentID,
-      });
-      if (replyLevel) throw new Error('답글을 더이상 작성할 수 없습니다.');
-    } catch (error) {
-      // 에러 생성
-      throw new DisableLevel(`service.comment.vaildateCommentLevel.${!!error.message ? error.message : 'Unknown_Error'}`);
-    }
-  }
-
-  /**
    *  write new comment to comment
    *  답글 작성를 작성합니다.
+   * @param reqData
+   * @returns Promise<Comments>
    */
   async writeNewCommentToComment(reqData: WriteNewCommentToCommentDTO): Promise<Comments> {
     try {
@@ -78,7 +63,7 @@ export class CommentsService {
         createdAt: Time.nowDate(),
       });
     } catch (error) {
-      // 에러 생성
+      if (error instanceof DisableLevel) throw error;
       throw new CommentToCommentWriteFailed(`service.comment.writnewcommenttocomment.${!!error.message ? error.message : 'Unknown_Error'}`);
     }
   }
@@ -96,30 +81,96 @@ export class CommentsService {
       throw new ViewAllCommentsFaild(`service.comment.viewpostcomments.${!!error.message ? error.message : 'Unknown_Error'}`);
     }
   }
-  // 코멘트를 수정합니다.
+  /**
+   * view one comments
+   * 특정 코멘트를 1개 반환합니다.
+   * @param commentID
+   * @returns Promise<Comments>
+   */
+  async viewOneComments(commentID: string): Promise<Comments> {
+    Logger.log('viewOneComments');
+    try {
+      return await this.commentsRepo.findOne({ id: commentID });
+    } catch (error) {
+      throw new ViewOneCommentFaild(`service.comment.viewonecomment.${!!error.message ? error.message : 'Unknown_Error'}`);
+    }
+  }
+
+  /**
+   * update comment
+   * 코멘트를 업데이트합니다.
+   * @param reqData
+   * @returns
+   */
   async updateComment(reqData: UpdateCommentDTO) {
+    Logger.log('updateComment');
     const { userID, commentID, contents } = reqData;
     try {
-      // 자신의 코멘트인지 확인
+      // 요청한 유저의 코멘트인지 확인
+      await this.isCommentOwner(userID, commentID);
       // 자신의 코멘트가 맞다면 코멘트 업데이트
-      // 틀리다면 에러
       return await this.commentsRepo.save({
         id: commentID,
         htmlContent: contents,
         updateAt: Time.nowDate(),
       });
     } catch (error) {
-      throw new UpdateCommentFaild(`service.comment.updatecomments.${!!error.message ? error.message : 'Unknown_Error'}`);
+      // not owner
+      if (error instanceof NotCommentOwner) throw error;
+      // not found comment
+      if (error instanceof ViewOneCommentFaild) throw error;
+      // update error
+      throw new UpdateCommentFaild(`service.comment.updatecomment.${!!error.message ? error.message : 'Unknown_Error'}`);
     }
   }
-  // 코멘트를 삭제합니다.
-  async deleteComment(commentID: string) {
+
+  /**
+   * soft delete comment
+   * 코멘트를 소프트 딜리트합니다.
+   * @param reqData
+   * @returns
+   */
+  async deleteComment(reqData: DeleteCommentDTO) {
+    const { userID, commentID } = reqData;
     try {
+      // 요청한 유저의 코멘트인지 확인
+      await this.isCommentOwner(userID, commentID);
+      // 코멘트 삭제
       return await this.commentsRepo.softRemove({
         id: commentID,
       });
     } catch (error) {
-      throw new DeleteCommentFaild(`service.comment.deletecomments.${!!error.message ? error.message : 'Unknown_Error'}`);
+      // not owner
+      if (error instanceof NotCommentOwner) throw error;
+      // not found comment
+      if (error instanceof ViewOneCommentFaild) throw error;
+      // delete error
+      throw new DeleteCommentFaild(`service.comment.deletecomment.${!!error.message ? error.message : 'Unknown_Error'}`);
+    }
+  }
+  /**
+   * vaildate comment level
+   * 코멘트의 레벨을 검증합니다.
+   * @param commentID
+   */
+  async vaildateCommentLevel(commentID: string): Promise<void> {
+    const { replyLevel } = await this.commentsRepo.findOne({
+      id: commentID,
+    });
+    if (replyLevel) throw new DisableLevel(`service.comment.vaildateCommentLevel.this comment is child`);
+  }
+  /**
+   * check comment owner
+   * 코멘트의 오너를 확인합니다.
+   * @param requestUserID
+   * @param commentID
+   * @returns Promise<Boolean>
+   */
+  async isCommentOwner(requestUserID: number, commentID: string): Promise<void> {
+    Logger.log('isCommentOwner');
+    const { usersId } = await this.viewOneComments(commentID);
+    if (usersId != requestUserID) {
+      throw new NotCommentOwner(`service.comment.iscommentowner.you are not owner`);
     }
   }
 }
