5791388eae31b1d58669f153e0e5db6e100c8dbb
MINJE-98 <jmj012100@gmail.com>
2021-11-10 18:27:06 +0900

    TIL-97 서비스  작성

--- src/src/user-blog-customization/user-blog-customization.service.ts
+++ src/src/user-blog-customization/user-blog-customization.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserblogCustomization } from 'src/entities/UserblogCustomization';
 import {
   CreateUserBlogCustomizationFailed,
+  DeleteUserBlogCustomizationFailed,
   GetUserBlogCustomizationFailed,
   UpdateUserBlogCustomizationFailed,
 } from 'src/ExceptionFilters/Errors/UserBlogCustomization/UserBlogCustomization.Error';
@@ -54,6 +55,7 @@ export class UserBlogCustomizationService {
   /**
    * update UserBlogCustomization
    * 유저의개인 블로그 설정 업데이트
+   *
    * @param updateUserBlogCustomizationDto
    * @returns
    */
@@ -69,6 +71,27 @@ export class UserBlogCustomizationService {
     } catch (error) {
       throw new UpdateUserBlogCustomizationFailed(
         `service.userblogcustomization.updateuserblogcustomization.${!!error.message ? error.message : 'Unknown_Error'}`,
+      );
+    }
+  }
+  /**
+   * delete UserBlogCustomization
+   * 유저의개인 블로그 설정 삭제
+   *
+   * @param userID
+   * @returns Promise<UserblogCustomization>
+   */
+  async deleteUserBlogCustomization(userID: number): Promise<UserblogCustomization> {
+    try {
+      return await this.userblogCustomizationRepo.save({
+        usersId: userID,
+        blogTitle: null,
+        statusMessage: null,
+        selfIntroduction: null,
+      });
+    } catch (error) {
+      throw new DeleteUserBlogCustomizationFailed(
+        `service.userblogcustomization.deleteuserblogcustomization.${!!error.message ? error.message : 'Unknown_Error'}`,
       );
     }
   }
