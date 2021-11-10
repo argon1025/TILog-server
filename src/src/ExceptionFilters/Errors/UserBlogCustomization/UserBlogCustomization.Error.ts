b12d05b0949bcff1c987e0a97a19058cd1525198
MINJE-98 <jmj012100@gmail.com>
2021-11-10 18:26:51 +0900

    TIL-97 에러 생성

--- src/src/ExceptionFilters/Errors/UserBlogCustomization/UserBlogCustomization.Error.ts
+++ src/src/ExceptionFilters/Errors/UserBlogCustomization/UserBlogCustomization.Error.ts
@@ -48,3 +48,19 @@ export class UpdateUserBlogCustomizationFailed implements Error {
     kr: '개인 블로그 설정 업데이트 실패하였습니다.',
   };
 }
+// 개인 블로그 설정 삭제 실패
+export class DeleteUserBlogCustomizationFailed implements Error {
+  // 개발자 코멘트를 생성자 매개변수로 할당할 수 있다.
+  constructor(public readonly description?) {}
+
+  // 미리 정의된 에러코드
+  public readonly codeNumber = 400;
+
+  public readonly codeText = 'DeleteUserBlogCustomizationFailed';
+
+  // 미리 정의된 메시지 객체
+  public readonly message = {
+    en: 'failed delete user-blog-customiztion',
+    kr: '개인 블로그 설정 삭제 실패하였습니다.',
+  };
+}
