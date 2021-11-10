c801767b400bab617d012bf6d2a645d6497c4442
MINJE-98 <jmj012100@gmail.com>
2021-11-10 16:38:55 +0900

    TIL-94 TypeOrmModule 추가

--- src/src/user-blog-customization/user-blog-customization.module.ts
+++ src/src/user-blog-customization/user-blog-customization.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserBlogCustomizationService } from './user-blog-customization.service';
 import { UserBlogCustomizationController } from './user-blog-customization.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserblogCustomization } from 'src/entities/UserblogCustomization';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserblogCustomization])],
   controllers: [UserBlogCustomizationController],
-  providers: [UserBlogCustomizationService]
+  providers: [UserBlogCustomizationService],
 })
 export class UserBlogCustomizationModule {}
