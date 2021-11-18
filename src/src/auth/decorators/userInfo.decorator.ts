4b7d9760839387a48d88ef820d5eec988ca8aa0b
MINJE-98 <jmj012100@gmail.com>
2021-11-18 20:05:25 +0900

    TIL-105 Edit stats -> userInfo 데코레이터 이름을 변경

--- src/src/auth/decorators/userInfo.decorator.ts
+++ src/src/auth/decorators/userInfo.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 // 유저 정보를 반환합니다.
-export const UserStats = createParamDecorator((data: string, ctx: ExecutionContext) => {
+export const UserInfo = createParamDecorator((data: string, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
   const user = request.user;
   return data ? user?.[data] : user;
