64d475e5fea98451d6da9107c566e91cb45d584b
MINJE-98 <jmj012100@gmail.com>
2021-12-19 21:22:52 +0900

    TIL-160 인증가드 에러 추가

--- src/src/auth/guard/auth.guard.ts
+++ src/src/auth/guard/auth.guard.ts
@@ -1,4 +1,5 @@
-import { CanActivate, ExecutionContext, Injectable, Logger } from '@nestjs/common';
+import { CanActivate, ExecutionContext, HttpException, Injectable, Logger } from '@nestjs/common';
+import { FailedAuthentication } from 'src/ExceptionFilters/Errors/Auth/Auth.error';
 
 // 인증 유무를 반환합니다
 @Injectable()
@@ -6,6 +7,10 @@ export class AuthenticatedGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
     Logger.log('AuthenticatedGuard');
+    if (!req.isAuthenticated()) {
+      const errorResponse = new FailedAuthentication(`auth.guard.authentication.failedauthentication`);
+      throw new HttpException(errorResponse, errorResponse.codeNumber);
+    }
     return req.isAuthenticated();
   }
 }
