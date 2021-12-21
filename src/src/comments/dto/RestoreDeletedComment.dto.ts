a8d086a12cd63594fccb20ed35445323a5724816
argon1025 <argon1025@gmail.com>
2021-12-22 00:17:28 +0900

    Delete 대소문자 오류 파일 삭제

--- src/src/comments/dto/RestoreDeletedComment.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { DeleteCommentDto } from './deleteComment.dto';
-
-export class RestoreDeletedCommentDto extends PartialType(DeleteCommentDto) {}
