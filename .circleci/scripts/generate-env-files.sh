bdacd2b7de0d22d16be932470b60c17094c83680
argon1025 <argon1025@gmail.com>
2021-12-21 20:28:15 +0900

    Add config

--- .circleci/scripts/generate-env-files.sh
+++ .circleci/scripts/generate-env-files.sh
@@ -10,5 +10,6 @@ done < "$SCRIPTS_PATH/env_ettings.txt"
 for value in "${mappings[@]}" ; do
  prefix=${value#*=};
  env_path=${value%=*};
+ echo "${prefix} ${env_path}"
  rm -f "$env_path" && for l in $(printenv | grep ^"$prefix"); do echo ${l#$prefix} >> "$env_path"; done
 done
