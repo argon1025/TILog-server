c19933f741eec5198ac5f107d69c144b43202d65
MINJE-98 <jmj012100@gmail.com>
2022-03-23 13:50:25 +0900

    feat: Sentry Module에 Interceptor, option 추가

--- src/src/sentry/sentry.module.ts
+++ src/src/sentry/sentry.module.ts
@@ -1,5 +1,26 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import * as Sentry from '@sentry/node';
+import { SentryInterceptor } from './sentry.interceptor';
 export const SENTRY_OPTIONS = 'SENTRY_OPTIONS';
 
 @Module({})
-export class SentryModule {}
+export class SentryModule {
+  static forRoot(options: Sentry.NodeOptions) {
+    Sentry.init(options);
+
+    return {
+      module: SentryModule,
+      providers: [
+        {
+          provide: SENTRY_OPTIONS,
+          useValue: options,
+        },
+        {
+          provide: APP_INTERCEPTOR,
+          useClass: SentryInterceptor,
+        },
+      ],
+    };
+  }
+}
